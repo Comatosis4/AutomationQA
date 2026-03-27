648d0ae5e88be04448556f53a12c66707338d948
faf <sf@gmail.com>
2026-03-27 19:09:21 +0200

    Виконані зміни 25

---
+++ lesson_25/homework25.py
@@ -0,0 +1,43 @@
+#XPATH
+#1. //*[text()='Home']
+#2. //*[text()='Sign up']
+#3. //*[text()='Contacts']
+#4. //body//div[contains(@class, 'container')]
+#5. //div[contains(@class,'video')]
+#6. //img[contains(@alt,'Instructions')]
+#7. //a[starts-with(@class,'btn')]
+#8. //section[starts-with(@class,'')]
+#9. (//*[starts-with(@class,'hero')])[5]
+#10. (//div[@class="container"])[3]
+#11. //div/div/app-header/header/div/div/div/a[@class='header_logo']
+#12. //body/app-root/app-global-layout/div/div/div/app-guest-layout/div/app-home/div/div/div/div/h2
+#13. //button[contains(@class,'header-link') and text()='About']
+#14. //h1[text()='Do more!']
+#15. //section[contains(@class,'hero')]//div[contains(@class,'container')]
+#16. //h1[text()='Do more!']/following::button[1]
+#17. //h1[text()='Do more!']/preceding::button[1]
+#18. //button[@appscrollto='aboutSection']
+#19. //button[contains(@class,'header-link') and text()='Contacts']
+#20. //div[@id='aboutSection']
+#21. //p[text()='Instructions and manuals']
+#22. (//p[contains(@class,'about-block_descr')])[2]
+#23. //div[@id='contactsSection']
+#24. //a[contains(@href,'facebook')]
+#25. (//a[contains(@class,'socials_link')])[3]
+
+#CSS
+#1. a.header-link
+#2. button.header-link[appscrollto="aboutSection"]
+#3. button.header-link[appscrollto="contactsSection"]
+#4. section.hero
+#5. section.hero h1
+#6. .hero-descriptor_btn
+#7. button.btn-primary
+#8. section.hero .container
+#9. .header.header
+#10. .section.hero .hero-descriptor button
+#11. .section.about
+#12. .about-block_title
+#13. img[src*="info_1"]
+#14. a[href*="facebook"]
+#15. a[href*="youtube"]
